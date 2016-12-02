2eb20816a3bea2352812e0df88aee17680ca7527
bradyt1 <bradytoon17@outlook.com>
2016-12-02 09:49:29 -0800

    Add files via upload

---
+++ code1/DecBin.java
@@ -0,0 +1,18 @@
+//DecBin.java 161027
+//NathanC
+class DecBin{
+	public static void main (String a[]){
+	
+	int n;
+	String bin; String hex;
+	
+	for (n = 0; n < 256; n ++){
+	
+				bin = Integer.toBinaryString(n);
+				System.out.print(n+" "+bin+"\n");
+				n++;
+				
+    }
+	
+}
+}	
